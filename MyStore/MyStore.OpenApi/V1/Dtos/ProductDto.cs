3f6ccb3fef23a45690a5e0d4cf1dd280b525cb39
tdieguez <tdieguez@fake.com>
2021-03-08 20:43:51 +0100

    Update dto to reflect entity data structure.

--- MyStore/MyStore.OpenApi/V1/Dtos/ProductDto.cs
+++ MyStore/MyStore.OpenApi/V1/Dtos/ProductDto.cs
@@ -5,6 +5,6 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
-        public string Category { get; set; }
+        public long CategoryId { get; set; }
     }
 }
