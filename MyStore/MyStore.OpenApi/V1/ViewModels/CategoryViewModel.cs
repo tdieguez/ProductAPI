aa37f21f3525464d53e812da3c95d263b6d748e7
tdieguez <tdieguez@fake.com>
2021-03-08 20:43:27 +0100

    View models.

---
+++ MyStore/MyStore.OpenApi/V1/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyStore.OpenApi.V1.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
